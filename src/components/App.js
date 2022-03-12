630ee4a442a21ebbd6ea6e8b66b3148410107883
Rajnish Kumar <Kumarrajnish363.rk@gmail.com>
2022-03-12 22:35:29 +0530

    Structure update

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="pane top-pane"></div>
+      <div className="pane">
+        <iframe title="output"></iframe>
+      </div>
+    </>
+  );
+}
+
+export default App;
